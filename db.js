229917f8931dfa48fa8c070e7e0c8a9d6c682259
PiliposyanDavid <Piliposyan.David99@gmail.com>
2020-04-01 02:07:17 +0400

    verj mery q*nem

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const {asValue} = require('@shahen.poghosyan/awilix');
 
 async function dbBootstrap(container) {
-    const dbConnectionString = "mongodb://david3ngeener:032dp333@ds219983.mlab.com:19983/heroku_zr0p08s0";
+    const dbConnectionString = "mongodb://heroku_zr0p08s0:4as6v9ahc4kp2dbahs7qrdmrpa@ds219983.mlab.com:19983/heroku_zr0p08s0";
 
     const connectionOptions = {
         autoIndex: false,
